3473e36c52db996dcf76ab30578dfbc5fea6a0f2
Lisa Shymkovich <95053643+LisaShymkovich@users.noreply.github.com>
2022-04-15 17:09:22 +0300

    Add files via upload

---
+++ armory.hpp
@@ -0,0 +1,24 @@
+#ifndef armory_hpp
+#define armory_hpp
+
+#include <stdio.h>
+#include <vector>
+#include "weapons.hpp"
+using namespace std;
+
+class armory 
+{
+public:
+    string armoryName;
+    armory(string aName);
+    int getPrice() const;
+    void addWeapon(Weapons* w);
+    void removeWeapon(Weapons* w);
+private:
+    virtual void dummy() {};
+    vector <Weapons*> weapons;
+};
+
+
+
+#endif /* armory_hpp */
